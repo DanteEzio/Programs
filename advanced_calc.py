b97e1f65d47c654d73ddb2ca4a61d9542a0ef2b7
Dante <quantum120@hotmail.com>
2020-06-01 20:42:02 -0500

    Primeros programas

---
+++ advanced_calc.py
@@ -0,0 +1,28 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+suma = number1 + number2
+resta = number1 - number2
+multiplicacion = number1 * number2
+division = number1 / number2
+division_exacta = number1 // number2
+potencia = number1 ** 2
+potencia2 = number2 ** 2
+potencia3 = number2 ** 3
+potencia4 = number1 ** 3
+modulo = number1 % number2
+
+
+
+print("Answer of the sum is: ",suma)
+print("Answer of the rest is: ",resta)
+print("Answer of the mult is: ",multiplicacion)
+print("Answer of the div is: ",division)
+print("Answer of the div_exacta is: ",division_exacta)
+print("Answer of the potencia **2 first number is: ",potencia)
+print("Answer of the potencia **2 second number is: ",potencia2)
+print("Answer of the potencia **3 first number is: ",potencia3)
+print("Answer of the potencia **3 second is: ",potencia4)
+print("Answer of the modulo is: ",modulo)
+
+
---
+++ basic_cal.py
@@ -0,0 +1,12 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+suma = number1 + number2
+resta = number1 - number2
+multiplicacion = number1 * number2
+division = number1 / number2
+
+print("Answer of the sum is: ",suma)
+print("Answer of the rest is: ",resta)
+print("Answer of the mult is: ",multiplicacion)
+print("Answer of the div is: ",division)
