45739fcb220eb40574559b333bb3514886882ce5
Dante <quantum120@hotmail.com>
2020-06-02 19:44:08 -0500

    News programs

--- conditionals.py
+++ conditionals.py
@@ -3,6 +3,6 @@ calif = float(input("Dame la calificacion final: "))
 #Saber si aprobe la materia o no (min. aprobaria ).
 
 if calif >= 6:
-    print("Aprobaste la materia, felicidades!")
+    print("You passed the subject, congratulations!")
 else:
-    print("Reprobaste la materia, lo siento.")
+    print("You failed the subject, im sorry.")
---
+++ promedio.py
@@ -0,0 +1,12 @@
+nombre = input("Enter your name: ")
+calif_1 = float(input("Enter the first grade: "))
+calif_2 = float(input("Enter the second grade: "))
+calif_3 = float(input("Enter the third grade: "))
+
+prom = (calif_1 + calif_2 + calif_3) / 3
+
+if prom >= 7:
+     print(nombre," you passed, congratulations!")
+else:
+    print(nombre," you failed, im sorry.")
+    
