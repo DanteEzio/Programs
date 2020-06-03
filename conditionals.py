293895cd144bad055f7088f789f2a1a2909b64d1
Dante <quantum120@hotmail.com>
2020-06-02 19:31:02 -0500

    Introduccion a condicionales

---
+++ conditionals.py
@@ -0,0 +1,8 @@
+calif = float(input("Dame la calificacion final: "))
+
+#Saber si aprobe la materia o no (min. aprobaria ).
+
+if calif >= 6:
+    print("Aprobaste la materia, felicidades!")
+else:
+    print("Reprobaste la materia, lo siento.")
